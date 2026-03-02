b8e1b4214355a03027c1c55ae040b04a2527488d
Karanvir Singh Rattan {KV} <karanrattan0420@gmail.com>
2026-03-01 23:45:25 -0700

    Add Upload route to the application

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import Upload from "./pages/Upload";
 import Home from "@/pages/home";
 import Feed from "@/pages/feed";
 import './App.css'
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Routes>
+      <Route path="/upload" element={<Upload />} />
         <Route path="/" element={<Home />} />
         <Route path="/feed" element={<Feed />} />
       </Routes>
